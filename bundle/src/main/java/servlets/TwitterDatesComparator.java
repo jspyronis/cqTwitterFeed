247aadd846374b42dfb0d104d50de6b626059bbf
John Spyronis <jspyronis@tacitknowledge.com>
2013-10-29 14:39:31 +0000

    Adding Twitter Dates Comparator - --

--- bundle/src/main/java/servlets/TwitterDatesComparator.java
+++ bundle/src/main/java/servlets/TwitterDatesComparator.java
@@ -1,10 +1,20 @@
 package servlets;
 
+import twitter4j.Status;
+
+import java.util.Comparator;
+
 /**
  * @author John S. (jspyronis@tacitknowledge.com)
  *         Date: 29/10/2013
  *         Time: 14:30
  */
-public class TwitterDatesComparator
+public class TwitterDatesComparator implements Comparator<Status>
 {
+
+    @Override
+    public int compare(Status o1, Status o2)
+    {
+        return o1.getCreatedAt().compareTo(o2.getCreatedAt());
+    }
 }
